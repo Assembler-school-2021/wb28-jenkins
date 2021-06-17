cd7bf43cc27f6b8f38a48d1980ef7e97dd2092a3
E.Sanz <enrique.sanz@secuoyas.com>
2021-06-17 20:09:42 +0200

    Fijamos la config necesaaria

--- djangotest/djangotest/settings.py
+++ djangotest/djangotest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f$=ad^+si_wmhh%w#t9%4bx^v)6u60bmz!l)i1ef#6ls#m*)(1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
---
+++ djangotest/settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'django_user',
+        'PASSWORD': 'vahKei4ovah0ieLasohaceiTahxeelae',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
